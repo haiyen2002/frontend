1659d9c0c106db50381ad0c6b53d674aaddb7dc4
haiyen2002 <tuoccho1995@gmail.com>
2022-02-18 16:43:52 +0700

    fix something

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import { userRequest } from './requestMethod'
+
 
 export const loginCall = async (userCredential, dispatch) => {
     dispatch({type: "LOGIN_START"});
     try {
-        const res = await axios.post("https://chaothubay.herokuapp.com/auth/login", userCredential)
+        const res = await userRequest.post("/auth/login", userCredential)
         dispatch({type: "LOGIN_SUCCESS", payload: res.data})
     } catch (error) {
         dispatch({type: "LOGIN_FAILURE", payload: error})
